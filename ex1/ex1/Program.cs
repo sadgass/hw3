401db174f26d56c7b6c391787244919c3343c516
sadgass <88186249+sadgass@users.noreply.github.com>
2021-08-10 15:46:19 +0500

    Add files via upload

---
+++ ex1/ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sum = 0;
+            string numbers = string.Empty;
+            
+
+            Console.WriteLine("Введите число");
+           
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            do
+            {
+                
+                if ((a % 2 != 0) && (a > 0))
+                {
+                    numbers = numbers + a +",";
+                    sum += a;
+
+                }
+
+                Console.WriteLine("Введите число");
+                a = Convert.ToInt32(Console.ReadLine());
+                
+
+
+            } while (a != 0);
+            Console.WriteLine(sum);
+            Console.WriteLine(numbers);
+
+        }
+
+        
+    }
+}
---
+++ ex2/ex2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        class Complex
+        {
+            public Complex Copy()
+            {
+                return new Complex(re: a, im: b);
+            }
+
+            public int a, b;
+
+            public Complex(int re, int im)
+            {
+                a = re;
+                b = im;
+            }
+
+
+            public static Complex operator +(Complex x, Complex y)
+            {
+
+                return new Complex(re: x.a + y.a, im: x.b + y.b);
+            }
+
+            public static Complex operator -(Complex x, Complex y)
+            {
+                return new Complex(re: x.a - y.a, im: x.b - y.b);
+            }
+            public static Complex operator *(Complex x, Complex y)
+            {
+                return new Complex(re:(x.a * y.a)-(x.b * y.b), im: (x.b * y.a)+(x.a * y.b));
+            }
+
+
+
+            public override string ToString()
+            {
+                return b >= 0 ? $"{a} + {b}i" : $"{a} - {-b}i";
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Complex complex1 = new Complex(5,11);
+            Complex complex2 = new Complex(11,27);
+           
+            Complex result = complex1+complex2;
+            Console.WriteLine( "(" + complex1.ToString()+ ")" + " + " + "(" + complex2.ToString() + ")" + " = "
+                +result.ToString());
+            result = complex1*complex2;
+            Console.WriteLine( "(" + complex1.ToString()+ ")" + " * " + "(" + complex2.ToString() + ")" + " = "
+               + result.ToString());
+            result = complex1-complex2;
+            Console.WriteLine( "(" + complex1.ToString()+ ")" + " - " + "(" + complex2.ToString() + ")" + " = "
+               + result.ToString());
+        }
+    }
+}
